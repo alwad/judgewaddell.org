df3870bcde7db3fa4f767b54b263b6bd15d0a0b8
alwad <alex@codestrike.com>
2015-06-11 09:49:22 -0400

    initial commit

---
+++ JudgeWaddell.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(judgewaddell.org.Startup))]
+namespace judgewaddell.org
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
