41a6415c63acd4523e3e69544b2c3b3842272db7
Alex Waddell <alex@codestrike.com>
2015-07-07 17:11:52 -0400

    ng-change

--- judgewaddell.org/app/controller/awardController.ts
+++ judgewaddell.org/app/controller/awardController.ts
@@ -2,15 +2,13 @@
 /// <reference path="../../scripts/typings/angularjs/angular-resource.d.ts" />
 /// <reference path="../../scripts/typings/angularjs/angular-route.d.ts" />
 
-function awardController($scope, $http) {
+function awardController($scope: IAwardScope, $http: angular.IHttpService) {
     $http.get('https://testsheets.apispark.net/v1/awards',
         {
             cache: true            
         })
-        .success(function(data) {
+        .success(function (data: IAward[]) {
             $scope.awards = data;
         });
-
-    $http.
 
 }
---
+++ judgewaddell.org/app/models/awardScope.ts
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../../scripts/typings/angularjs/angular-resource.d.ts" />
+/// <reference path="../../scripts/typings/angularjs/angular-route.d.ts" />
+
+interface IAwardScope extends angular.IScope {
+    awards: IAward[]
+}
+
+interface IAward {
+    awardDate: Date,
+    scholar: string,
+    desc: string,
+    link: string
+}
