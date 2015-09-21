85eebd9c29a1206fdec4b1cb1bd7bc72ce012d30
Alex Waddell <alex@codestrike.com>
2015-09-21 14:01:01 -0400

    more pages

---
+++ judgewaddell.org/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace judgewaddell.org.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [HttpGet]
+        [ActionName("404")]
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+
+    }
+}
