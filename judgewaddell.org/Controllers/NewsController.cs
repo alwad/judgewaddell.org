1728d3179e61c8fe84c573682adb14998a12841c
alwad <alwad>
2017-01-12 12:19:25 -0500

    New content

---
+++ judgewaddell.org/Controllers/NewsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace judgewaddell.org.Controllers
+{
+    public class NewsController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
